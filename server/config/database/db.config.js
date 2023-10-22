bd4bb407fc81eda5dc11f8394acaf1c8f26ff74b
Jessica Zarate <jeca.zarate.up@phinmaed.com>
2023-10-22 12:21:44 +0800

    dotenv config

--- server/config/database/db.config.js
+++ server/config/database/db.config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "purplesql",
-  DB: "amoredb",
-  dialect: "mysql",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: 'mysql',
   pool: {
     max: 5,
     min: 0,
@@ -11,3 +13,9 @@ module.exports = {
     idle: 10000,
   },
 };
+
+// do an npm i dotenv then add a .env file in server and pass this data
+// DB_HOST = localhost;
+// DB_USER = root;
+// DB_PASSWORD = "";
+// DB_NAME = amoredb;
